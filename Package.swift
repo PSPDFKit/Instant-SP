69859fd9b2df23bbc2f6cf666b29c3ac1eb7b3e8
RadAzzouz <rad@pspdfkit.com>
2020-12-08 08:05:13 -0500

    Update for PSPDFKit 10.1.1 for iOS

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instant",
-            url: "https://customers.pspdfkit.com/instant/xcframework/10.1.0.zip",
-            checksum: "d89c5014d8005b023ab634bd1d9008715e4a5124a97b76ae9e9b2fb2488b7733"),
+            url: "https://customers.pspdfkit.com/instant/xcframework/10.1.1.zip",
+            checksum: "a299bc9fbbc7fdd03b98e5deee1a112c8c29c420976673df145a8d0a13a0490f"),
     ]
 )
