3bb54589292b07b04455fc91b49559505f1391a4
Stefan Kieleithner <stefan.kieleithner@gmail.com>
2024-01-24 08:38:33 +0100

    Update for PSPDFKit 13.3.0 for iOS

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instant",
-            url: "https://customers.pspdfkit.com/instant/xcframework/13.2.0.zip",
-            checksum: "64439b220f6c885c7bb69bbd7051f5d7e11597194be18d39c1e8088cc7d222dd"),
+            url: "https://customers.pspdfkit.com/instant/xcframework/13.3.0.zip",
+            checksum: "eaedb12700f7a1922f444792211a87211426683617533474a627100d74dd4ddc"),
     ]
 )
