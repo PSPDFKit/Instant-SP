414aef709ab32ef1b379dfe914e267a9e628162e
Stefan Kieleithner <stefan.kieleithner@gmail.com>
2024-09-16 11:06:55 +0200

    Update for PSPDFKit 14.0.0 for iOS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instant",
-            url: "https://customers.pspdfkit.com/instant/xcframework/13.9.1.zip",
-            checksum: "cf7d0c3cfb47cf285bd009780bfbe13c7aa42348be23f5085eb8e09dbc64f061"),
+            url: "https://customers.pspdfkit.com/instant/xcframework/14.0.0.zip",
+            checksum: "bc9f1138bc19138164671440ec11265106804de997178957b1ba546fb140820e"),
     ]
 )
