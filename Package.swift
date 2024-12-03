281fa47c891b7e89f1ed8f8ec6db8376e1f2341e
Douglas Hill <hello@douglashill.co>
2024-12-03 11:24:46 +0000

    Update for PSPDFKit 14.2.1 for iOS

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instant",
-            url: "https://customers.pspdfkit.com/instant/xcframework/14.2.0.zip",
-            checksum: "cff6e8867ed361a49d3d174f7f6989de40aab867469627f16a61ef3de54c1880"),
+            url: "https://customers.pspdfkit.com/instant/xcframework/14.2.1.zip",
+            checksum: "ef33991d996f581cd79ff8eba899e871ef495055ae09c3d8a6540b161710e3d7"),
     ]
 )
