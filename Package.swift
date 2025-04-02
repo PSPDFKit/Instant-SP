101308378802cdf1c85d3fe2e6134193b825f31b
Douglas Hill <hello@douglashill.co>
2025-04-02 16:44:02 +0100

    Update for Nutrient iOS SDK 14.7.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instant",
-            url: "https://customers.pspdfkit.com/instant/xcframework/14.6.0.zip",
-            checksum: "960697b0967f40e3975ff2fdff6e5e4744beb765dad1532353e70b7ab62cbdaf"),
+            url: "https://customers.pspdfkit.com/instant/xcframework/14.7.0.zip",
+            checksum: "303db55622d80afae1bae3b8aa078c2722bd243e7b0474be612dfd210238d7d4"),
     ]
 )
